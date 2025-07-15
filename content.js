30281ece1c41945db332b2db20fd8451575b4612
Jasper On <onj@wit.edu>
2025-07-15 13:39:54 -0400

    fix2

--- content.js
+++ content.js
@@ -1,8 +1,12 @@
 chrome.storage.sync.get(['darkModeEnabled'], (data) => {
-  if (data.darkModeEnabled !== false) {  // default ON
+  // Default ON (if undefined or true)
+  if (data.darkModeEnabled !== false) {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = chrome.runtime.getURL('dm.css');
     document.head.appendChild(link);
+    console.log('Dark mode CSS injected');
+  } else {
+    console.log('Dark mode disabled by user');
   }
 });
